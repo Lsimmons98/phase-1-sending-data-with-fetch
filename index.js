99868be65f7a9b8b6787e394c8edc1d5dbb42002
Liam Simmons <liam.simmons@powerhrg.com>
2024-05-16 16:05:04 -0400

    Add user to server and post ID to dom

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+  const paragraph = document.createElement('p');
+
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      'name': name,
+      'email': email
+    })
+  })
+  .then(resp => resp.json())
+  .then(user => {
+    document.querySelector('body').appendChild(paragraph).textContent = user.id
+  })
+  .catch(() => document.querySelector('body').appendChild(paragraph).textContent = 'FetchError: request to http://localhost:3000/users failed, reason: Unauthorized Access')
+}
+
+
+ 
+  
+
